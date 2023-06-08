0b344432bed2d9be8452af37bdb68e28fc0ba760
jdvramos <jonndenzelramos@gmail.com>
2023-06-08 18:55:58 +0800

    second commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
+const BASE_URL = "https://talking-dreams-backend.onrender.com";
+// const BASE_URL = "http://localhost:3500";
 // const BASE_URL = "http://192.168.1.4:3500";
 
 export default axios.create({
