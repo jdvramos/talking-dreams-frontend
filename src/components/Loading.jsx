d462fc9eacbe1cb249cf28ac61f135153cc62746
jdvramos <jonndenzelramos@gmail.com>
2023-06-12 18:26:09 +0800

    eleventh commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,4 +1,5 @@
-import { Box, styled, CircularProgress } from "@mui/material";
+import { Box, styled, CircularProgress, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const CenteredBox = styled(Box)(({ theme }) => ({
     height: "100%",
@@ -8,9 +9,29 @@ const CenteredBox = styled(Box)(({ theme }) => ({
 }));
 
 const Loading = () => {
+    const [showMessage, setShowMessage] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setShowMessage(true);
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
         <CenteredBox>
-            <CircularProgress />
+            {showMessage ? (
+                <Box width="90%" textAlign="center" color="text.primary">
+                    <Typography variant="body1" mb={2}>
+                        The server is getting ready to serve your request.
+                        Please hold on for a moment.
+                    </Typography>
+                    <CircularProgress />
+                </Box>
+            ) : (
+                <CircularProgress />
+            )}
         </CenteredBox>
     );
 };
